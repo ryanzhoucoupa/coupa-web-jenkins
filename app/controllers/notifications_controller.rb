8da91b8d6d8046c71f54f3470f667e8bb946e0b3
Ryan Zhou <ryan.zhou@coupa.com>
2017-10-18 21:22:53 -0700

    remove select

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -81,7 +81,7 @@ class NotificationsController < ApplicationController
     github_login = params.fetch(:github_login, nil)
 
     if github_login
-      notifications = User.find_by(github_login: github_login).notifications.select(:data).order(:id)
+      notifications = User.find_by(github_login: github_login).notifications.order(:id)
       render json: notifications.map { |n| { ghprbPullId: n.pr_id, data: JSON.parse(n.data) } }
       #render json: notifications.map(&:data).reduce([]) { |final, c| final += JSON.parse(c);  final; }
       return
