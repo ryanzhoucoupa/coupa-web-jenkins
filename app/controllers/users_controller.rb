78a06dbc9cc56ffd096b83e3a4e43d4549dc73c7
Ryan Zhou <ryan.zhou@coupa.com>
2017-10-19 15:37:54 -0700

    removed additonal json format

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -71,9 +71,9 @@ class UsersController < ApplicationController
     @user = User.find_by(github_login: github_login)
 
     if @user.update(user_params)
-      format.json { render json: @user }
+      render json: @user
     else
-      format.json { render json: @user.errors, status: :unprocessable_entity }
+      render json: @user.errors, status: :unprocessable_entity
     end
   end
 
